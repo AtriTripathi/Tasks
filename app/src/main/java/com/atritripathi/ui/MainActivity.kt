dd577c8d23ab30ad282a679a3c0ecd7f2f3b7650
AtriTripathi <tripathi.atri@gmail.com>
2021-03-26 08:44:28 +0530

    Setup NavGraph

--- app/src/main/java/com/atritripathi/ui/MainActivity.kt
+++ app/src/main/java/com/atritripathi/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.atritripathi.tasks
+package com.atritripathi.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.atritripathi.tasks.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/atritripathi/ui/tasks/TasksFragment.kt
@@ -0,0 +1,9 @@
+package com.atritripathi.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.atritripathi.tasks.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
+
+
+}
